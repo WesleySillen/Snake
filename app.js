160c563cebb7317ac8ae9d2a7d0e5b0ae8b974c7
WesleySillen <wesley.sillen1@gmail.com>
2025-07-18 12:07:18 +0200

    Code cleanup

--- app.js
+++ app.js
@@ -67,7 +67,6 @@ function createFood() {
   }
   foodX = randomFood(0, gameWidth - unitSize);
   foodY = randomFood(0, gameWidth - unitSize);
-  console.log(foodX);
 }
 function drawFood() {
   ctx.fillStyle = foodColor;
@@ -96,17 +95,18 @@ function drawSnake() {
 }
 function changeDirection(event) {
   const keyPressed = event.keyCode;
-  console.log(keyPressed);
 
   const LEFT = 37;
-  const UP = 38;
-  const RIGHT = 39;
-  const DOWN = 40;
-
-  const goingUp = yVelocity == unitSize;
-  const goingDown = yVelocity == -unitSize;
-  const goingRight = xVelocity == unitSize;
   const goingLeft = xVelocity == -unitSize;
+
+  const UP = 38;
+  const goingUp = yVelocity == unitSize;
+
+  const RIGHT = 39;
+  const goingRight = xVelocity == unitSize;
+
+  const DOWN = 40;
+  const goingDown = yVelocity == -unitSize;
 
   switch (true) {
     case keyPressed == LEFT && !goingRight:
